3537edebcb1847e22ceb72971b35d363eb5d3702
hannahmaung <hannahmaung33@gmail.com>
2021-06-06 16:45:30 -0700

    adding calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+"""
+calculator app using circle ci
+"""
+
+
+def add(a, b):
+    return a+b
+
+
+def subtract(a, b):
+    return a-b
+
+
+def multiply(a, b):
+    return a*b
+
+
+def divide(a, b):
+    return a/b
+
+
